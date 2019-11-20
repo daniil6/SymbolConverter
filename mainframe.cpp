f754a8b51a6aee6d7cc41f99c4cd0fb44c14d33e
Daniil <rbkwork@yandex.ru>
2019-11-20 17:58:21 +0300

    Add main process convert across button

--- mainframe.cpp
+++ mainframe.cpp
@@ -3,25 +3,65 @@
 CMainFrame::CMainFrame(wxFrame* frame)
     : wxFrame(frame, wxID_ANY, wxT("SymbolConverter"))
 {
-    const wxString allow[] = { wxT(".") wxT("0"), wxT("1"), wxT("2"), wxT("3"), wxT("4"), wxT("5"), wxT("6"), wxT("7"),
-        wxT("8"), wxT("9") };
+    const wxString allow[] = { wxT("0"), wxT("1"), wxT("2"), wxT("3"), wxT("4"), wxT("5"), wxT("6"), wxT("7"), wxT("8"),
+        wxT("9"), wxT("."), wxT("-") };
     wxArrayString* str = new wxArrayString(sizeof(allow) / sizeof(allow[0]), allow);
     wxTextValidator* validTxt = new wxTextValidator(wxFILTER_INCLUDE_CHAR_LIST);
     validTxt->SetIncludes(*str);
 
-    wxButton* btnConvert = new wxButton(this, NewControlId(), wxT("Convert"));
-    wxTextCtrl* txtView =
-        new wxTextCtrl(this, NewControlId(), wxT(""), wxDefaultPosition, wxSize(200, 250), wxTE_MULTILINE);
-    txtView->SetValidator(*validTxt);
+    wxButton* convertButton = new wxButton(this, NewControlId(), wxT("Convert"));
+    m_inTextCtrl = new wxTextCtrl(this, NewControlId(), wxT(""), wxDefaultPosition, wxSize(200, 250), wxTE_MULTILINE);
+    m_outTextCtrl = new wxTextCtrl(this, NewControlId(), wxT(""), wxDefaultPosition, wxSize(200, 250), wxTE_MULTILINE);
+    m_inTextCtrl->SetValidator(*validTxt);
+    m_outTextCtrl->SetValidator(*validTxt);
 
     wxBoxSizer* main_box = new wxBoxSizer(wxVERTICAL);
-    main_box->Add(btnConvert);
-    main_box->Add(txtView);
+    main_box->Add(convertButton);
+
+    wxBoxSizer* h_box = new wxBoxSizer(wxHORIZONTAL);
+    h_box->Add(m_inTextCtrl);
+    h_box->Add(m_outTextCtrl);
+    main_box->Add(h_box);
+
     SetSizerAndFit(main_box);
 
-    this->SetSize(500, 350);
+    Bind(wxEVT_BUTTON, &CMainFrame::OnConvert, this, convertButton->GetId());
+    m_inTextCtrl->Bind(wxEVT_CHAR_HOOK, &CMainFrame::OnKeyDown, this);
 }
 
 CMainFrame::~CMainFrame()
 {
+}
+
+void CMainFrame::OnKeyDown(wxKeyEvent& event)
+{
+    if(wxGetKeyState(wxKeyCode('.')) == true)
+        if(m_inTextCtrl->GetValue().Find('.') != -1)
+            return;
+
+    if(wxGetKeyState(wxKeyCode('-')) == true)
+        if(m_inTextCtrl->GetValue().empty() == false)
+            return;
+
+    event.Skip();
+}
+
+void CMainFrame::OnConvert(wxCommandEvent& event)
+{
+    std::string strParse;
+    std::string strIn = m_inTextCtrl->GetValue().ToStdString();
+
+    std::vector<double> out;
+    for(auto itr = strIn.begin(); itr != strIn.end() + 1; ++itr) {
+        if(*itr == '\n' || *itr == '\0') {
+            out.push_back(ConvertStringToDouble(strParse));
+            strParse.clear();
+        } else
+            strParse.push_back(*itr);
+    }
+
+    for(auto& p : out) {
+        wxString str(ConvertDoubleToBit(p));
+        m_outTextCtrl->SetValue(str);
+    }
 }
--- mainframe.h
+++ mainframe.h
@@ -1,10 +1,18 @@
 #ifndef CMAINFRAME_H
 #define CMAINFRAME_H
 
+#include <RBKLib.h>
 #include <wx/wx.h>
 
 class CMainFrame : public wxFrame
 {
+private:
+    wxTextCtrl* m_inTextCtrl;
+    wxTextCtrl* m_outTextCtrl;
+
+    void OnConvert(wxCommandEvent& event);
+    void OnKeyDown(wxKeyEvent& event);
+
 public:
     CMainFrame(wxFrame* frame);
     ~CMainFrame();
